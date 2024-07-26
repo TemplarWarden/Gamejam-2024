e685b06bfb6ebb9ba3b1f46da83db349a27d7d7f
TemplarWarden <daniel.louis.raynor@gmail.com>
2024-07-26 17:22:17 +1200

    Menu Sound Effects
    Menu Sound Effects added and bugfixed

--- Gamejam 2024/assets/scenes/mainmenu/main_menu.gd
+++ Gamejam 2024/assets/scenes/mainmenu/main_menu.gd
@@ -16,36 +16,42 @@ var has_navigate_sounds = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	AudioManager.play_music(menu_music)
+
+	has_navigate_sounds = false
 	%PlayButton.grab_focus()
 	%ContinueButton.set_visible(GameManager.has_running_game)
 	if GameManager.has_running_game:
-		%ContinueButton.grab_focus()
+		%ContinueButton.grab_focus()	
+	has_navigate_sounds = true
 
 func _on_continue_button_pressed():
 	has_navigate_sounds = false
 	get_tree().change_scene_to_file(game_scene)
-	_run_sfx(AudioID.CONFIRM)
+	run_sfx(AudioID.CONFIRM)
 
 func _on_play_button_pressed():
 	has_navigate_sounds = false
 	GameManager.reset_manager()
 	GameManager.has_running_game = true
-	_run_sfx(AudioID.CONFIRM)
-	get_tree().change_scene_to_file(game_scene)
+	run_sfx(AudioID.CONFIRM)
+	change_scene()
 
 func _on_quit_button_pressed():
 	has_navigate_sounds = false
-	_run_sfx(AudioID.CANCEL, true)
+	run_sfx(AudioID.CANCEL, true)
 	get_tree().quit()
 
 func _button_navigate() -> void:
 	pass	
 
-func _run_sfx(type: AudioID, will_wait: bool = false) -> bool:
+func change_scene():
+	get_tree().change_scene_to_file(game_scene)
+
+func run_sfx(type: AudioID, will_wait: bool = false) -> bool:
 	var ref = AudioManager.play_sound_effect_instance(sfxdict[type])
 	if will_wait: await ref.finished
 	return will_wait
 
 func _button_exited() -> void:
-	if has_navigate_sounds: _run_sfx(AudioID.NAVIGATE)
+	if has_navigate_sounds:
+		run_sfx(AudioID.NAVIGATE)
--- Gamejam 2024/assets/ui/pausemenu/pause_menu.gd
+++ Gamejam 2024/assets/ui/pausemenu/pause_menu.gd
@@ -1,28 +1,44 @@
 extends CanvasLayer
 
+enum AudioID {CONFIRM, NAVIGATE, CANCEL}
+
+@export var sfxdict = {
+	AudioID.CONFIRM: "res://audio/sfx/menuconfirm.wav",
+	AudioID.NAVIGATE: "res://audio/sfx/menunavigate.wav",
+	AudioID.CANCEL: "res://audio/sfx/menuexit.wav"
+}
+
+var has_navigate_sounds: bool = true
 var main_menu_scene:String = "res://assets/scenes/mainmenu/mainmenu.tscn"
 var is_paused = false
 @onready var previous_focus: Control = %ResumeButton
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	has_navigate_sounds = false
 	pause(false)
 
 func _on_resume_button_pressed():
+	has_navigate_sounds = false
+	run_sfx(AudioID.CANCEL)
 	pause(false)
 
 func _on_menu_button_pressed():
+	has_navigate_sounds = false
+	run_sfx(AudioID.CONFIRM)
 	get_tree().change_scene_to_file(main_menu_scene)
 
 func _on_quit_button_pressed():
+	has_navigate_sounds = false
+	run_sfx(AudioID.CANCEL)
 	get_tree().quit()
 
 func _input(event):
 	if event.is_action_pressed("cui_menu"):
 		pause(!is_paused)
 	
-
 func pause(passedbool: bool) -> void:
+	has_navigate_sounds = false
 	get_tree().paused = passedbool
 	var current_focus
 	if passedbool:
@@ -30,6 +46,17 @@ func pause(passedbool: bool) -> void:
 		current_focus = %ResumeButton
 	else:
 		current_focus = previous_focus
-	current_focus.grab_focus()	
+	if is_instance_valid(current_focus): current_focus.grab_focus()	
+	has_navigate_sounds = true
 	is_paused = passedbool
 	visible = passedbool
+	
+func run_sfx(type: AudioID, will_wait: bool = false) -> bool:
+	var ref = AudioManager.play_sound_effect_instance(sfxdict[type])
+	if will_wait: await ref.finished
+	return will_wait
+
+func _button_exited() -> void:
+	if has_navigate_sounds:
+		print_debug("focus loss")
+		run_sfx(AudioID.NAVIGATE)
